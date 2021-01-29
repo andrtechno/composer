5bcbbd7f3b0b4fdd5132f2cc8f4e8e3810dc623b
Andrew <andrew.panix@gmail.com>
2021-01-29 09:49:06 +0200

    Update PixelionInstaller.php

--- Installers/PixelionInstaller.php
+++ Installers/PixelionInstaller.php
@@ -8,15 +8,7 @@ class PixelionInstaller extends BaseInstaller
         'module'           => 'modules/{$name}/',
         'theme'            => 'web/themes/{$name}/',
         'library'          => 'libraries/{$name}/',
-        'profile'          => 'profiles/{$name}/',
-        'database-driver'  => 'drivers/lib/Drupal/Driver/Database/{$name}/',
-        'drush'            => 'drush/{$name}/',
         'custom-theme'     => 'themes/custom/{$name}/',
         'custom-module'    => 'modules/custom/{$name}/',
-        'custom-profile'   => 'profiles/custom/{$name}/',
-        'drupal-multisite' => 'sites/{$name}/',
-        'console'          => 'console/{$name}/',
-        'console-language' => 'console/language/{$name}/',
-        'config'           => 'config/sync/',
     );
 }
