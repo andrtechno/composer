8be94c241b4370e3326a702985290d5a3882e766
Andrew <andrew.panix@gmail.com>
2021-02-01 14:59:17 +0200

    Update PixelionInstaller.php

--- Installers/PixelionInstaller.php
+++ Installers/PixelionInstaller.php
@@ -6,9 +6,11 @@ class PixelionInstaller extends BaseInstaller
 {
     protected $locations = array(
         'module' => 'vendor/{$vendor}/{$name}/',
+        'component' => 'vendor/{$vendor}/{$name}/',
         'widget' => 'vendor/{$vendor}/{$name}/',
         'theme' => 'web/themes/{$name}/',
         'module-custom' => 'modules/{$name}/',
         'widget-custom' => 'widgets/{$name}/',
+        'widget-component' => 'components/{$name}/',
     );
 }
