40d5c5821070ec2ff53cfe76f50bdea64ec88bdd
Andrew <andrew.panix@gmail.com>
2021-05-21 18:24:07 +0300

    Update Installer.php

--- Installers/Installer.php
+++ Installers/Installer.php
@@ -547,7 +547,7 @@ class Installer extends LibraryInstaller
         fclose($handle);
         echo "\n";
         echo "\e[36mThank you.\e[0m\n";
-        echo "\e[36mNext step run command:\"cmd migrate\".\e[0m\n";
+        echo "Next step run command:\e[36m\"cmd migrate\"\e[0m.\n";
 
     }
 }
